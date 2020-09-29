2b91636c10824b1f2e2f5e667c083a70721e97a4
pintti <allu.tuovinen@gmail.com>
2020-09-29 17:57:58 +0300

    slice functions ready

--- Encryption/Paloittelu_ja_kanta.py
+++ Encryption/Paloittelu_ja_kanta.py
@@ -63,24 +63,26 @@ def count_number_down(number, z):
 
 def turn_number_to_word(alphabet, lista, k):
     word = []
-    print(lista)
     for numbers in lista:
         for i in range(k):
-            number = numbers[]
+            number = int(numbers[i*2]) * 10
+            number += int(numbers[i*2+1]) - 1
+            while number >= len(alphabet):
+                number -= len(alphabet)
+            word.append(alphabet[number])
     return word
 
 
 def slice_ecnrypt_method(alphabet, word, z, a, b):
     k = solve_starting_number(len(alphabet), z)
     number_list = turn_word_to_numbers(word, alphabet, k)
+    print(number_list)
     for i, number in enumerate(number_list):
         nu_number = count_number_down(affini_letter_change(a, b, number), z)
         number_list[i] = str(nu_number).zfill(k*2)
     nu_word = turn_number_to_word(alphabet, number_list, k)
+    print(number_list)
     print(nu_word)
     
 
-slice_ecnrypt_method(english, 'takeitaway', 2773, 17, 329)
-    
-        
-
+slice_ecnrypt_method(english, 'material', 5657, 30, 0)
