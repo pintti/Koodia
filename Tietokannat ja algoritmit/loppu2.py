3d9279694e18d4f9ff9ed0d395ffb809b4058770
pintti <allu.tuovinen@gmail.com>
2020-11-05 18:57:10 +0200

    finished some work

---
+++ Tietokannat ja algoritmit/loppu2.py
@@ -0,0 +1,131 @@
+library = {
+    "nmbr_of_cities": 0,
+    "nmbr_of_roads": 0,
+    "end_city": 0,
+    'cities': {}
+}
+
+
+def open_file(file):
+    """"Opens file, cleans up the newlines from text and returns the text for further
+    work"""
+    while True:
+        try:
+            with open(file, "r") as f:
+                text = f.readlines()
+                for i, line in enumerate(text):
+                    text[i] = line.strip().split(' ')
+            return text
+        except FileNotFoundError:
+            print('Such file does not exist.')
+    
+
+def check_lines(text):
+    """Function checks that lines are right and inputs them into library"""
+    cities, roads = text.pop(0)
+    library["nmbr_of_cities"], library["nmbr_of_roads"] = int(cities), int(roads)
+    library["end_city"] = int(text.pop(-1)[0])
+    for line in text:
+        try:
+            city, neighbour, height = line
+            create_city(int(city), int(neighbour), int(height))
+        except ValueError:
+            print("City values not usable")
+
+
+def create_city(city, neighbour, road_height):
+    """Functions creates cities in the library"""
+    if str(city) not in library['cities'].keys():
+        library['cities'][str(city)] = []
+    if str(neighbour) not in library['cities'].keys():
+        library['cities'][str(neighbour)] = []
+    library['cities'][str(city)].append((neighbour, road_height))
+    library['cities'][str(neighbour)].append((city, road_height))
+
+
+def range_check(cities, city, n=0, checked_cities=None):
+    """Function checks the range to goal"""
+    if checked_cities == None:
+        checked_cities = []
+    ranges = []
+    if city == 1:
+        return n
+    for next_city, _ in cities[str(city)]:
+        if next_city == 1:
+            return n+1
+        elif next_city not in checked_cities:
+            checked_cities.append(next_city)
+            nest = range_check(cities, next_city, n+1, checked_cities)
+            if nest:
+                ranges.append(nest)
+    checked_cities = []
+    if ranges:
+        return min(ranges)
+
+
+def find_road(cities):
+    """This is where the magic happens"""
+    roads = []
+    city = library['end_city']
+    for next_city, next_road in cities[str(city)]:
+        roads.append([city, next_city, next_road])
+    while city != 1:
+        index = find_small(roads)
+        future = roads[index]
+        future.pop(-1)
+        del roads[index]
+        for next_city, next_road in cities[str(future[-1])]:
+            if next_city not in future:
+                new_road = future + [next_city, next_road]
+                roads.append(new_road)
+        city = future[-1]
+    return future
+
+
+def find_small(roads):
+    small = None
+    for value in roads:
+        if small:
+            if value[-1] < small:
+                small = value[-1]
+                index = roads.index(value)
+        else:
+            small = value[-1]
+            index = roads.index(value)
+    return index
+
+
+def turn_road(old_road):
+    fixed_road = []
+    for i in range(len(old_road)):
+        fixed_road.append(old_road[-1-i])
+    return fixed_road
+
+
+def find_peak(cities, road):
+    try:
+        i = 1
+        peak = None
+        for number in road:
+            for value in cities[str(number)]:
+                if value[0] == road[i]:
+                    if not peak:
+                        peak = value[1]
+                    elif peak < value[1]:
+                        peak = value[1]
+                i += 1
+    except IndexError:
+        return peak
+
+
+
+if __name__ == "__main__":
+    text = open_file("text.txt")
+    check_lines(text)
+    if range_check(library['cities'], library['end_city']):
+        inv_road = find_road(library['cities'])
+        real_road = turn_road(inv_road)
+        peak = find_peak(library['cities'], real_road)
+        print("Route: ", inv_road, " with height of ", peak, "meters.")
+    else:
+        print('No roads to destination available.')
--- Tietokannat ja algoritmit/lopputyo.py
+++ Tietokannat ja algoritmit/lopputyo.py
@@ -7,7 +7,12 @@ library = {
 
 memory = {
     'road_taken': [],
-    'road_to_think': []
+    'road_to_think': [],
+    'highest_road_taken': 0,
+    'steps': 0,
+    'dont': [],
+    'second': [], 
+    'true_road': []
 }
 
 
@@ -46,30 +51,102 @@ def create_city(city, neighbour, road_height):
     library['cities'][str(city)].append((neighbour, road_height))
     library['cities'][str(neighbour)].append((city, road_height))
 
+
 def pretty_printer(printed):
     for value in printed:
         print(value, end=': ')
         print(printed[value][0], printed[value][1])
 
 
-def count_roads(cities, start):
-    next_city, next_height = cities[start][0]
-    memory['road_to_think'] = cities[start][1]
-    while start != 1:
-        memory['road_taken'] = [start]
-        for city, height in cities[start]:
-            if height < next_height:
-                next_city = city
-                next_height = height
-            elif memory['road_to_think'][1] < height:
-                memory['road_to_think'] = (city, height)
-        if next_city not in memory['road_taken']:
-            start = next_city
+def first_step(cities, start):
+    memory['road_taken'].append(int(start))
+    city, road = cities[start][0]
+    for next_city, next_road in cities[start]:
+        if road < next_road:
+            next_city, next_road = city, road
         else:
-            
+            if not memory['second']:
+                memory['second'] = (next_city, next_road)
+            elif memory['second'][1] > next_road:
+                memory['second'] = (next_city, next_road, memory['steps'])
+    memory['highest_road_taken'] = next_road
+    count_roads(cities, next_city, range_check(cities, next_city, 0), int(start))
+
+
+def count_roads(cities, start, n, last_city=0):
+    memory['road_taken'].append(start)
+    memory['steps'] += 1
+    print('START ', start)
+    print(memory['highest_road_taken'])
+    if start != 1:
+        next_road = 1000
+        print(cities[str(start)])
+        for city, road in cities[str(start)]:
+            if city != last_city and city not in memory['dont']:
+                print(city, road)
+                if road <= memory['highest_road_taken'] and road < next_road:
+                    """nu_n = range_check(cities, city)
+                    if nu_n <= n:
+                        n = nu_n
+                        next_city, next_road = city, road"""
+                    next_city, next_road = city, road
+                #elif road < next_road:
+                #    next_city, next_road = city, road
+                elif not memory['second']:
+                    memory['second'] = (city, road, memory['steps'])
+                elif memory['second'][1] > road:
+                    memory['second'] = (city, road, memory['steps'])
+        print(memory['second'])
+        if next_road > memory['highest_road_taken']:
+            memory['highest_road_taken'] = next_road
+        if memory['second']:
+            if next_road > memory['second'][1]:
+                for i in range(memory['second'][2], memory['steps']):
+                    memory['dont'].append(memory['road_taken'].pop(-1))
+                next_city = memory['road_taken'].pop(-1)
+                memory['steps'] = memory['steps'] - memory['second'][2]
+                memory['highest_road_taken'] = memory['second'][1]
+                memory['second'] = []
+                count_roads(cities, next_city, range_check(cities, next_city))
+            else:
+                count_roads(cities, next_city, range_check(cities, next_city), start)
+        else:
+            count_roads(cities, next_city, range_check(cities, next_city), start)
+
+
+
+def range_check(cities, city, n=0, checked_cities=None):
+    if checked_cities == None:
+        checked_cities = []
+    ranges = []
+    if city == 1:
+        return n
+    for next_city, _ in cities[str(city)]:
+        if next_city == 1:
+            return n+1
+        elif next_city not in checked_cities:
+            checked_cities.append(next_city)
+            nest = range_check(cities, next_city, n+1, checked_cities)
+            if nest:
+                ranges.append(nest)
+    checked_cities = []
+    if ranges:
+        return min(ranges)
+
+
+
+def turn_road(old_road, fixed_road):
+    for value in range(len(old_road)):
+        fixed_road.append(old_road[-1+value])
+    
 
 
 if __name__ == "__main__":
     text = open_file("text.txt")
     check_lines(text)
-    count_roads(library['cities'], str(library['end_city']))
+    if range_check(library['cities'], library['end_city']):
+        first_step(library['cities'], str(library['end_city']))
+        turn_road(memory['road_taken'], memory['true_road'])
+        print(memory['true_road'], memory['highest_road_taken'])
+    else:
+        print('No roads to destination available.')
