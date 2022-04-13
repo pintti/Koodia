146bacdd3e27b2966e798811866615cbf46d4153
pintti <allu.tuovinen@gmail.com>
2022-04-13 12:52:13 +0300

    scrape scrape

--- webScraper.py
+++ webScraper.py
@@ -5,7 +5,13 @@ def get(html):
     req = r.get(html)
     if req.status_code == 200:
         soup = bs(req.content, "html.parser")
-        print(soup)
+        posts = soup.find(id="posts")
+        bodies = posts.find("div", class_="page")
+        bodies = bodies.find_all(class_='tborder')
+        for body in bodies:
+            print(body.prettify())
+            input()
+
         
 
 get("https://www.city-data.com/forum/health-wellness/3245374-have-you-had-covid-vaccine-side.html")
