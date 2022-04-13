b93f3218a87e9504d6b10aeeeb04170ee090d248
pintti <allu.tuovinen@gmail.com>
2022-04-13 13:36:55 +0300

    scraped

--- webScraper.py
+++ webScraper.py
@@ -1,15 +1,21 @@
 import requests as r
 from bs4 import BeautifulSoup as bs
+import re
 
 def get(html):
     req = r.get(html)
     if req.status_code == 200:
         soup = bs(req.content, "html.parser")
         posts = soup.find(id="posts")
-        bodies = posts.find("div", class_="page")
-        bodies = bodies.find_all(class_='tborder')
+        bodies = posts.find_all('div', id=re.compile("^edit"))
         for body in bodies:
-            print(body.prettify())
+            #print(body.prettify())
+            name = body.find(class_="bigusername").text
+            rep = body.find(class_="smallfont").text
+            text = body.find('div', id=re.compile('^post_message_')).text
+            print(name)
+            print(rep)
+            print(text)
             input()
 
         
